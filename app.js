7ef08324db464831b68d6acaa8cd4dd00de794ff
lgirao <lesleygirao@gmail.com>
2021-09-19 14:28:16 -0400

    added name attribute to name and email inputs

--- app.js
+++ app.js
@@ -8,18 +8,27 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// BODY PARSER MIDDLEWARE
-app.use(express.urlencoded({ extended: true }));
-
 // STATIC FOLDER - PUBLIC (style, js, img)
 app.use(express.static(path.join(__dirname, 'public')));
+
+// BODY PARSER MIDDLEWARE
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index')
  });
+
+app.post('/send', (req, res) => {
+    res.send(req.body);
+});
+
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
  });
+
+
+ 
