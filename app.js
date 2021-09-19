4edbd180fea9d870d63a0fbd15d0c716f2cbd31a
lgirao <lesleygirao@gmail.com>
2021-09-19 18:56:41 -0400

    added dotenv module, gitignore

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const result = require('dotenv').config();
 const path = require('path');
 const nodemailer = require('nodemailer');
 
@@ -15,6 +16,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if (result.error) {
+    throw result.error
+  };
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -22,8 +26,51 @@ app.get('/', (req, res) => {
  });
 
 app.post('/send', (req, res) => {
-    res.send(req.body);
+    const output = `
+        <p>You have a new contact request</p>
+        <h3>Contact Details</h3>
+        <ul>
+            <li>Name: ${req.body.name}</li>
+            <li>Email: ${req.body.email}</li>
+            <li>Message: ${req.body.message}</li>
+        </ul>
+    `;
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: process.env.YOUR_HOST,
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: process.env.NODEMAILER_ID, // A2 Hosting Email
+        pass: process.env.NODEMAILER_PASSWORD, // Password for email account
+      },
+      tls:{
+        rejectUnauthorized:false
+      }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+      from: '"Website Contact" <test@waylandballoonfest.com>', // sender address
+      to: "lesleygirao@gmail.com", // list of receivers
+      subject: "Balloonfest Contact", // Subject line
+      text: "Hello world", // plain text body
+      html: output, // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log("Message sent: %s", info.messageId);   
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        res.render('index');
+    });
 });
+
 
 
 app.listen(3000, () => {
